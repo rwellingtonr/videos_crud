82b0a5f70f598ca9a0d6ee09a6ceb5e584f2c409
rwellingtonr <wellington-158@hotmail.com>
2021-12-18 08:48:22 -0300

    created db migrations

---
+++ src/database/migrations/1639826196764-createCategories.ts
@@ -0,0 +1,21 @@
+import { Column, MigrationInterface, QueryRunner, Table } from "typeorm"
+
+export class createCategories1639826196764 implements MigrationInterface {
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.createTable(
+			new Table({
+				name: "categories",
+				columns: [
+					{ name: "id", type: "uuid", isPrimary: true },
+					{ name: "name", type: "varchar", isUnique: true },
+					{ name: "descriptions", type: "varchar" },
+					{ name: "created_at", type: "timestamp", default: "now()" },
+				],
+			})
+		)
+	}
+
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.dropTable("categories")
+	}
+}
---
+++ src/database/migrations/1639827073259-createVideos.ts
@@ -0,0 +1,31 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm"
+
+export class createVideos1639827073259 implements MigrationInterface {
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.createTable(
+			new Table({
+				name: "videos",
+				columns: [
+					{ name: "id", type: "uuid", isPrimary: true },
+					{ name: "name", type: "varchar", isUnique: true },
+					{ name: "descriptions", type: "varchar" },
+					{ name: "category_id", type: "uuid" },
+					{ name: "duration", type: "numeric" },
+					{ name: "created_at", type: "timestamp", default: "now()" },
+				],
+				foreignKeys: [
+					{
+						name: "fk_videos_category",
+						columnNames: ["category_id"],
+						referencedTableName: "categories",
+						referencedColumnNames: ["id"],
+					},
+				],
+			})
+		)
+	}
+
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.dropTable("videos")
+	}
+}
